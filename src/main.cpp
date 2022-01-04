538e0fdbdff0676d7fbd04e5601831658438d1f1
Krzysztof Pęczek <krzysztofpeczek@protonmail.com>
2022-01-04 13:23:58 +0100

    Finalized glew setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <GL/glew.h>
 
 int main()
 {
-    std::cout << "hello world" << "\n";
+    GLenum err = glewInit();
+    if(GLEW_OK != err)
+    {
+        std::cerr << "Error: " << "\n";
+        std::cerr << glewGetErrorString(err) << "\n";
+        return 1;
+    }
+    std::cout << "Using glew: " << glewGetString(GLEW_VERSION) << "\n";
     return 0;
 }
